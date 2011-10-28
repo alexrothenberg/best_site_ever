b1263b2c0c4eb0732439d759d4ad292f80eb1b11
Alex Rothenberg <alex@alexrothenberg.com>
2011-10-28 10:38:57 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestSiteEver::Application.config.secret_token = 'b843074aba994f1192c79fac31a6bb8f2b0089b8f14e0d682c476ae9175b502a2a1c07a487f82a4407bdb01ead713737030b738a4ed5c2946c9fd5afec4871c2'
